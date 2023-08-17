a0001ff730d1c71a004d55d0b712d9c12dde8867
kiet-lta <ltakiet96@gmail.com>
2023-08-18 01:21:33 +0700

    feat(calculator): funcs max, average, pnd, sum

--- calculator/client/client.go
+++ calculator/client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 
 	"github.com/1412270/grpc-go/calculator/calculatorpb"
@@ -18,7 +19,10 @@ func main() {
 
 	client := calculatorpb.NewCalculatorServiceClient(cc)
 	// log.Printf("service client %f", client)
-	callSum(client)
+	// callSum(client)
+	// callPND(client)
+	// callAverage(client)
+	callMax(client)
 }
 
 func callSum(c calculatorpb.CalculatorServiceClient) {
@@ -32,4 +36,115 @@ func callSum(c calculatorpb.CalculatorServiceClient) {
 		log.Fatalf("call sum api error %v", err)
 	}
 	log.Printf("sum api Response %v", resp.GetResult())
+}
+
+func callPND(c calculatorpb.CalculatorServiceClient) {
+	stream, err := c.PrimeNumberDecomposition(context.Background(), &calculatorpb.PNDRequest{
+		Number: 50,
+	})
+
+	if err != nil {
+		log.Fatalf("callPND err %v", err)
+	}
+
+	for {
+		resp, recvErr := stream.Recv()
+		if recvErr == io.EOF {
+			log.Println("server finish streaming")
+			return
+		}
+
+		log.Printf("prime number is %v", resp.GetResult())
+	}
+}
+
+func callAverage(c calculatorpb.CalculatorServiceClient) {
+	log.Println("sending nums to cal average...")
+	stream, err := c.Average(context.Background())
+	if err != nil {
+		log.Fatalf("err while cal average %v", err)
+	}
+
+	listReq := []calculatorpb.AverageRequest{
+		{
+			Num: 6,
+		},
+		{
+			Num: 8,
+		},
+		{
+			Num: 9.7,
+		},
+		{
+			Num: 5,
+		},
+	}
+
+	for _, req := range listReq {
+		err := stream.Send(&req)
+		if err != nil {
+			log.Fatalf("err while send num request %v", err)
+		}
+	}
+
+	resp, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("err while receive average %v", err)
+	}
+
+	log.Printf("average response %v", resp.Result)
+}
+
+func callMax(c calculatorpb.CalculatorServiceClient) {
+	log.Println("call finding max...")
+	stream, err := c.Max(context.Background())
+	if err != nil {
+		log.Fatalf("err while cal max %v", err)
+	}
+
+	waitChannel := make(chan struct{})
+
+	go func() {
+		listReq := []calculatorpb.MaxRequest{
+			{
+				Num: 6,
+			},
+			{
+				Num: 8,
+			},
+			{
+				Num: 9,
+			},
+			{
+				Num: 7,
+			},
+		}
+		for _, req := range listReq {
+			err := stream.Send(&req)
+			if err != nil {
+				log.Fatalf("err while send num request %v", err)
+				break
+			}
+		}
+
+		stream.CloseSend()
+	}()
+
+	go func() {
+		for {
+			resp, err := stream.Recv()
+			if err == io.EOF {
+				log.Println("find max complete")
+				break
+			}
+			if err != nil {
+				log.Fatalf("err while recv %v", err)
+				break
+			}
+			log.Printf("max is: %v\n", resp.GetNum())
+		}
+		close(waitChannel)
+	}()
+
+	<-waitChannel
 }
--- calculator/server/server.go
+++ calculator/server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"net"
 
@@ -21,6 +22,75 @@ func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculat
 	}
 
 	return resp, nil
+}
+
+func (*server) PrimeNumberDecomposition(req *calculatorpb.PNDRequest,
+	stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer) error {
+	k := int32(2)
+	N := req.GetNumber()
+	for N > 1 {
+		if N%k == 0 {
+			N = N / k
+			stream.Send(&calculatorpb.PNDResponse{
+				Result: k,
+			})
+		} else {
+			k++
+		}
+	}
+	return nil
+}
+
+func (*server) Average(stream calculatorpb.CalculatorService_AverageServer) error {
+	log.Println("average calculating...")
+	var total float32
+	var count int
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			resp := &calculatorpb.AverageResponse{
+				Result: total / float32(count),
+			}
+
+			return stream.SendAndClose(resp)
+		}
+		if err != nil {
+			log.Fatalf("err while cal avarage %v", err)
+		}
+
+		log.Printf("receive num %v", req.GetNum())
+		total += req.GetNum()
+		count++
+	}
+}
+
+func (*server) Max(stream calculatorpb.CalculatorService_MaxServer) error {
+	log.Println("find max calling...")
+	max := int32(0)
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			log.Println("EOF...")
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("err while recv %v", err)
+			return err
+		}
+
+		num := req.GetNum()
+		if num > max {
+			max = num
+		}
+		err = stream.Send(&calculatorpb.MaxResponse{
+			Num: max,
+		})
+		if err != nil {
+			log.Fatalf("err while find max %v", err)
+			return err
+		}
+	}
+
 }
 
 func main() {
